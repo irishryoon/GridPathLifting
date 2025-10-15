bd6af0385de379e8e3f12d1ec799be8950ce3c34
jared <yaojaredz@outlook.com>
2025-10-14 21:27:12 -0400

    Refactor trajectory recovery and comparison functions to improve clarity and update terminology to 'Reconstruction Error'

--- toroidal_coordinates/Gardner_toroidal.py
+++ toroidal_coordinates/Gardner_toroidal.py
@@ -81,7 +81,45 @@ def Gardner_coord(sspikes: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarr
         numpy.ndarray: The centcosall matrix.
         numpy.ndarray: The centsinall matrix.
     """
-    persistence = Gardner_persistence(sspikes)
+    dim = 6
+    ph_classes = [0,1] # Decode the ith most persistent cohomology class
+    num_circ = len(ph_classes)
+    dec_tresh = 0.99
+    metric = 'cosine'
+    coeff = 47
+    active_times = 15000
+    k = 1000
+    num_times = 5
+    n_points = 1200
+    nbs = 800
+    # sigma = 1500
+            
+    num_neurons = len(sspikes[0,:])
+            
+    times_cube = np.arange(0,len(sspikes[:,0]),num_times)
+    movetimes = np.sort(np.argsort(np.sum(sspikes[times_cube,:],1))[-active_times:])
+    movetimes = times_cube[movetimes]
+
+    dim_red_spikes_move_scaled,__,__ = pca(preprocessing.scale(sspikes[movetimes,:]), dim = dim)
+    indstemp,dd,fs  = sample_denoising(dim_red_spikes_move_scaled,  k, 
+                                        n_points, 1, metric)
+    dim_red_spikes_move_scaled = dim_red_spikes_move_scaled[indstemp,:]
+    X = squareform(pdist(dim_red_spikes_move_scaled, metric))
+    knn_indices = np.argsort(X)[:, :nbs]
+    knn_dists = X[np.arange(X.shape[0])[:, None], knn_indices].copy()
+    sigmas, rhos = smooth_knn_dist(knn_dists, nbs, local_connectivity=0)
+    rows, cols, vals = compute_membership_strengths(knn_indices, knn_dists, sigmas, rhos)
+    result = coo_matrix((vals, (rows, cols)), shape=(X.shape[0], X.shape[0]))
+    result.eliminate_zeros()
+    transpose = result.transpose()
+    prod_matrix = result.multiply(transpose)
+    result = (result + transpose - prod_matrix)
+    result.eliminate_zeros()
+    d = result.toarray()
+    d = -np.log(d)
+    np.fill_diagonal(d,0)
+
+    persistence = ripser(d, maxdim=2, coeff=coeff, do_cocycles= True, distance_matrix = True)   
     
     ############ Decode cocycles ################
     diagrams = persistence["dgms"] # the multiset describing the lives of the persistence classes
@@ -134,7 +172,8 @@ if __name__ == '__main__':
     rat_name, mod_name, sess_name, day_name = ('R', '1', 'OF', 'day2')
     sspikes,xx,yy,__,__ = get_spikes(rat_name, mod_name, day_name, sess_name, bType = 'pure',
                                             bSmooth = True, bSpeed = True, folder = folder )
-    # Gardner_coord(sspikes)
+    print(sspikes.shape)
+    Gardner_coord(sspikes)
     persistence = Gardner_persistence(sspikes)
     import pdb; pdb.set_trace()
     plt.show()
